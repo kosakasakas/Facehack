ed2cea4a37ef4fd9a3f871a98ef85ba8447f77c4
takahiro-kosaka <takahiro.kosaka@dena.com>
2016-06-29 13:10:23 +0900

    一旦修正

--- src/ofMorphableModel.cpp
+++ src/ofMorphableModel.cpp
@@ -8,12 +8,39 @@
 
 #include "ofApp.h"
 #include "ofMorphableModel.hpp"
-#include "vtkStandardMeshRepresenter.h"
-#include "StatisticalModel.h"
 #include "memory.h"
-#include <boost/scoped_ptr.hpp>
 
-using namespace statismo;
+#include <itkCommand.h>
+#include <itkEuclideanDistancePointMetric.h>
+#include <itkLevenbergMarquardtOptimizer.h>
+#include <itkMesh.h>
+#include <itkMeshFileReader.h>
+#include <itkPointSetToPointSetRegistrationMethod.h>
+
+#include "itkStandardMeshRepresenter.h"
+#include "itkStatisticalModel.h"
+#include "itkStatisticalShapeModelTransform.h"
+
+
+const unsigned Dimensions = 3;
+typedef itk::Mesh<float, Dimensions  > MeshType;
+
+typedef itk::StandardMeshRepresenter<float, Dimensions> RepresenterType;
+
+typedef itk::MeshFileReader<MeshType> MeshReaderType;
+typedef itk::EuclideanDistancePointMetric<MeshType, MeshType> MetricType;
+
+// As a transform, we use the StatisticalShapeModelTransform, that comes with statismo
+typedef itk::StatisticalShapeModelTransform<MeshType, double, Dimensions> TransformType;
+
+
+typedef itk::PointSetToPointSetRegistrationMethod<MeshType, MeshType> RegistrationFilterType;
+
+typedef  itk::LevenbergMarquardtOptimizer OptimizerType;
+
+typedef itk::StatisticalModel<MeshType> StatisticalModelType;
+
+
 
 ofMorphableModel::ofMorphableModel()
 {}
@@ -26,12 +53,12 @@ bool ofMorphableModel::Initialize()
     ofDirectory dataDir("unpublic");
     string dataPath = dataDir.getAbsolutePath() + "/model2009-publicmm1-bfm.h5";
     
-    typedef vtkStandardMeshRepresenter RepresenterType;
-    typedef StatisticalModel<vtkPolyData> StatisticalModelType;
-    
-    RepresenterType* representer = RepresenterType::Create();
-     boost::scoped_ptr<StatisticalModelType> model(StatisticalModelType::Load(representer, dataPath.c_str()));
-    std::cout << "loaded model with " << model->GetNumberOfPrincipalComponents() << " Principal Components" << std::endl;
-    
+    // load the model
+    //RepresenterType::Pointer representer = RepresenterType::New();
+    //StatisticalModelType::Pointer model = StatisticalModelType::New();
+    //model->Load(representer, dataPath.c_str());
+    //MeshType::Pointer fixedPointSet  = model->GetRepresenter()->GetReference();
+    //std::cout << "model succesully loaded " << std::endl;
+  
     return true;
 }
